3f215835952ceead4ca83186a0f800eda8729e74
John Doe <fffelagund@gmail.com>
2020-02-11 12:57:48 +0200

    anaconda cloud attempt

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name="liveprint",
+    version="0.1.0",
+    description="Python utility library for dynamic animations projections",
+    classifiers=[
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="projections animation projector",
+    url="http://github.com/monomonedula/liveprint",
+    author="Vladyslav Halchenko",
+    author_email="valh@tuta.io",
+    license="Apache License Version 2.0",
+    packages=["liveprint"],
+    test_suite="nose.collector",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    install_requires=["methodtools", "networkx"]
+)
